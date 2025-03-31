0b6aba875d5ff93dc9c848befa19c28e740c186d
rafduqcol <rafduqcol@alum.us.es>
2025-03-31 11:20:35 +0200

    feat: add protection to routes in the frontend

--- www/js/utils/player_auth.js
+++ www/js/utils/player_auth.js
@@ -1,5 +1,5 @@
-const isAdmin = localStorage.getItem('user').isAdmin === 'true';
-if (!isAdmin) {
+const player = localStorage.getItem('user')
+if (!player) {
     alert("You are not authorized to access this page.");
     window.location.href = "http://127.0.0.1:8000/www/views/index.html"; 
 }
